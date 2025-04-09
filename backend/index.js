7a3a5feb21eb7b8332343365c8c75d9c1c944648
Stakshi <semwaleishta6@gmail.com>
2025-04-09 16:32:50 +0530

    change ip

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT=5000;
 const path =require ('path');
 
 app.use(cors());
-app.use(cors({origin:"http://localhost:5173", credentials:true}));  
+app.use(cors({origin:"https://bookstorefrontend-f3gf.onrender.com", credentials:true}));  
 app.use(express.json());
 connectDB();
 
